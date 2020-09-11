56b9f9590b5f469c024a28c65696cd8b51a18938
Carlos Franco <carlosboni.sa@gmail.com>
2020-09-11 14:36:27 -0300

    removed isnan and isinf

--- Converter/Restrictions.py
+++ Converter/Restrictions.py
@@ -5,51 +5,37 @@ def dIin_max(obj, X):
     dIin = obj.calculated_values['dIin']
     Iin = obj.calculated_values['Iin']
     res = (obj.design_features['dIin_max']*Iin - dIin)
-    if math.isnan(res) or math.isinf(res):
-        res = -1
     return res
 
 
 def bmax_Li(obj, X):
     res = (obj.design_features['Bmax']['EntranceInductor'] - obj.calculated_values['BmaxLi'])
-    if math.isnan(res) or math.isinf(res):
-        res = -1
     return res
 
 
 def AeAw_Li(obj, X):
     res = obj.entrance_inductor.Core.AeAw - obj.calculated_values['LiIrms']*obj.entrance_inductor.Ncond*obj.entrance_inductor.Cable.S/(obj.design_features['Jmax']*obj.safety_params['ku']['EntranceInductor'])
-    if math.isnan(res) or math.isinf(res):
-        res = -1
     return res
 
 
 def JLi(obj, X):
     res = obj.design_features['Jmax'] - obj.calculated_values['LiIrms']/(obj.entrance_inductor.Cable.Scu*obj.entrance_inductor.Ncond)
-    if math.isnan(res) or math.isinf(res):
-        res = -1
     return res
 
 
 # Auxiliary Inductor #
 def bmax_Lk(obj, X):
     res = (obj.design_features['Bmax']['AuxiliaryInductor'] - obj.calculated_values['BmaxLk'])
-    if math.isnan(res) or math.isinf(res):
-        res = -1
     return res
 
 
 def AeAw_Lk(obj, X):
     res = obj.auxiliary_inductor.Core.AeAw - obj.calculated_values['TransformerIrms'][0]*obj.entrance_inductor.Ncond*obj.auxiliary_inductor.Cable.S/(obj.design_features['Jmax']*obj.safety_params['ku']['AuxiliaryInductor'])
-    if math.isnan(res) or math.isinf(res):
-        res = -1
     return res
 
 
 def JLk(obj, X):
     res = obj.design_features['Jmax'] - obj.calculated_values['TransformerIrms'][0]/(obj.auxiliary_inductor.Cable.Scu*obj.auxiliary_inductor.Ncond)
-    if math.isnan(res) or math.isinf(res):
-        res = -1
     return res
 
 def ZVS_restriction(converter, X):
@@ -66,8 +52,6 @@ def ZVS_restriction(converter, X):
     k3 = (2*n*Vo/(Ro*(1-D)) - Po/Vi + Vi*D/(2*X[0]*X[1]))**2
     k4 = max(k1/k2, k1/k3)
     res = X[2] - k4
-    if math.isnan(res) or math.isinf(res):
-        res = -1
     return res
 
 #TransformerRestrictions = [Transformer_Core_Loss, Transformer_Cable_Loss]
@@ -107,3 +91,20 @@ def Lk_restriction_s2(converter, Vi, D, L, fs):
     k2 = (2*n*Vo/(Ro*(1-D)) - Po/Vi + Vi*D/(2*L*fs))**2
 
     return k1/k2
+
+
+def Gain_Restriction_Term(converter):
+    Vo = converter.design_features['Vo']
+    Ro = converter.design_features['Ro']
+    n = converter.transformer.Ratio
+
+    keys = ['Min', 'Max']
+    Dbound = [converter.design_features['D']['Max']+0.01, converter.design_features['D']['Max']-0.01]
+
+    term = 1e10
+    for [D, key] in zip(Dbound, keys):
+        Vi = converter.design_features['Vi'][key]
+        new_term = Ro*D**2*(1-D)*(Vi*n - Vo*(1-D))/(Vo*((2*D-1)**2+1)*n**2)
+        if new_term < term:
+            term = new_term
+    return term
